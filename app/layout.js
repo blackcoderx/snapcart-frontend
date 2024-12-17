a008530447ba977b71535c806533a75102dd23d5
blackocoderx <ziggahemmanuel99@gmail.com>
2024-12-17 23:57:28 +0000

    added the login page, rtk query and redux statemangement

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import StoreProvider from "../redux/StoreProvider";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -18,12 +8,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <StoreProvider>
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {children}
       </body>
     </html>
+    </StoreProvider>
+
   );
 }
