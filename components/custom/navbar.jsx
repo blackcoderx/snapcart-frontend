f2da0b4bfd19e0c89f218fe8d48aeb0789782b0c
Ernest Ampem <ernestampem12008@gmail.com>
2024-12-29 10:40:56 +0000

    working on the navigation bar

--- components/custom/navbar.jsx
+++ components/custom/navbar.jsx
@@ -4,53 +4,59 @@
 import {useSelector} from "react-redux";
 import Link from 'next/link'
 import Image from 'next/image'
+import styles from "../styles/header.module.css"
 const links = ['Home', 'Explore', 'Cart', 'Chat']
 
-// const NotLogIn = () => {
-//
-//     // you can design these links the once from the design i sent you
-//     return (
-//         <ul className='flex gap-4'>
-//             <li key='login'>
-//                 <Link href='/login'>Login</Link>
-//             </li>
-//             <li key='signup'>
-//                 <Link href='/signup'>Sign Up</Link>
-//             </li>
-//         </ul>
-//     )
-// }
-//
-// const LogIn = ({username, profile_pic}) => {
-//     return (
-//         <div>
-//             <Image
-//                 src={`https://res.cloudinary.com/duoxbuhpw/${profile_pic}`}
-//                 alt={user_name}
-//                 width={40}
-//                 height={40}
-//                 className="rounded-full" />
-//
-//             <p> {username} </p>
-//         </div>
-//
-//     )
-// }
+const NotLogIn = () => {
+
+    // you can design these links the once from the design i sent you
+    return (
+        <ul className='flex gap-4 justify'>
+            <li key='login' className={styles.li}>
+                <Link href='/login'>Login</Link>
+            </li>
+            <li key='signup' className={styles.signup}>
+                <Link href='/signup'>Sign Up</Link>
+            </li>
+        </ul>
+    )
+}
+
+const LogIn = ({username, profile_pic}) => {
+    return (
+        <div>
+            <Image
+                src={`https://res.cloudinary.com/duoxbuhpw/${profile_pic}`}
+                alt={user_name}
+                width={40}
+                height={40}
+                className="rounded-full" />
+
+            <p> {username} </p>
+        </div>
+
+    )
+}
 
 export default function Navbar() {
     const {isAuthenticated, user_name, profile_pic} = useSelector(state => state.auth);
 
     return (
-        <nav className="navbar flex">
+        <nav className="navbar flex gap-4 justify-center">
+        <div>
             <ul className='flex gap-4'>
                 {links.map(link => (
-                    <li key={link}>
+                    <li key={link} className={styles.li}>
                         <Link href={`/${link.toLowerCase()}`}>{link}</Link>
                     </li>
                 ))}
-            </ul>
+            </ul>    
+        </div>   
 
-            {/*{isAuthenticated? <LogIn username={user_name} profile_pic={profile_pic} /> : <NotLogIn />}*/}
+        <div>
+            {isAuthenticated? <LogIn username={user_name} profile_pic={profile_pic} /> : <NotLogIn/>}
+        </div>
+        
         </nav>
 
     )
